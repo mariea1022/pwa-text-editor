f1f784850d812ddec4754aec5497b42c4d1c7500
Marie Lewis <mariea1022@gmail.com>
2023-02-19 07:59:48 -0800

    added logic to put and get data from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,45 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the database');
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('contact', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('contact', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('contact');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  // how to add an error message here?
+  // console.error('putDb not implemented');
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const contactDb = await openDB('jate', 1);
+
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+  // how to add an error message here?
+  // console.error('getDb not implemented');
+}
 
 initdb();
